2c4c9254a06f1eee5470bed5e7b9e6923df65cca
MrChest <zookeeper.uob@outlook.com>
2025-09-23 21:20:22 +0800

    ASDFASDFASDFASDF

---
+++ K/bul.js
@@ -0,0 +1,16 @@
+export class bul{
+    static spd=40;
+    constructor(drct,x,y){
+        this.tex=new Image();
+        this.tex2=new Image();
+        this.tex3=new Image();
+        this.tex.src="bul.png";
+        this.drct=drct-0.5*Math.PI;
+        this.x=x;
+        this.y=y;
+    }
+    move(){
+        this.x+=Math.cos(1*this.drct)*bul.spd;
+        this.y+=Math.sin(1*this.drct)*bul.spd;
+    }
+}
--- K/main.js
+++ K/main.js
@@ -1,6 +1,12 @@
+import { bul } from "./bul.js";
 
+let tex=new Image();
+tex.src="skin.png";
+const spd=5;
+const rsp=0.5* Math.PI / 180;
+let drct=0;
 
-
+let buls=[];
 
 let bk=document.querySelector("#bk");
 let pen = bk.getContext("2d");
@@ -8,7 +14,7 @@ let grd= new Image();
 grd.src="ground.png";
 let gx=960;
 let gy=540;
-const cs=960;
+const cx=960;
 const cy=540;
 const gl=128;
 let ax=960;
@@ -22,28 +28,58 @@ document.addEventListener("keyup", (e) => {
 });
 
 function mainloop(){
-    if(keys["w"]&&gy<90*gl){
-        gy+=5;
-    }
-    if(keys["a"]&&gx<90*gl){
-        gx+=5;
-    }
-    if(keys["s"]&&gy>-90*gl){
-        gy-=5;
-    }
-    if(keys["d"]&&gx>-90*gl){
-        gx-=5;
-    }
+    pen.clearRect(0, 0, bk.width, bk.height);//print
     for(let i=-100;i<100;i++){
         for(let j=-100;j<100;j++){
-            let tx=gx+i*gl;
-            let ty=gy+j*gl;
-            if(tx>-100&&tx<1930&&ty>-100&&ty<1090){
-                pen.drawImage(grd,tx,ty,gl,gl);
+            let tx=i*gl;
+            let ty=j*gl;
+            if(gx+tx>-200&&gx+tx<1930&&gy+ty>-200&&gy+ty<1090){
+                pen.drawImage(grd,gx+tx,gy+ty,gl,gl);
             }
         }
     }
-    // console.log(gx,gy);
+    pen.save();
+    pen.translate(cx, cy);
+    pen.rotate(drct);
+    pen.drawImage(tex, -0.5 * tex.width, -0.5 * tex.height, tex.width, tex.height);
+    pen.restore();
+    for(let i=0;i<buls.length;i++){
+        if(gx+buls[i].x+cx>-200&&gx+buls[i].x+cx<2000&&gy+buls[i].y+cy>-200&&gy+buls[i].y+cy<2000){
+            pen.drawImage(buls[i].tex,gx+buls[i].x-0.5*buls[i].tex.width+cx,gy+buls[i].y-0.5*buls[i].tex.height+cy,buls[i].tex.width,buls[i].tex.height);
+        }
+    }
+
+
+    gy+=Math.cos(-1*drct)*spd;//move
+    gx+=Math.sin(-1*drct)*spd;
+    if(gy>90*gl){
+        gy-=gl
+    }
+    if(gx>90){
+        gx-=gl
+    }
+    if(gy<-90*gl){
+        gy+=gl;
+    }
+    if(gx<-90*gl){
+        gx+=gl;
+    }
+    ax=-gx;
+    ay=-gy;
+    if(keys["d"]){
+        drct+=rsp;
+    }
+    if(keys["a"]){
+        drct-=rsp;
+    }
+    if(keys[" "]){
+        let tmp=new bul(drct,ax,ay);
+        buls.push(tmp);
+    }
+    for(let i=0;i<buls.length;i++){
+        buls[i].move();
+    }
+    console.log(gx,gy);
     requestAnimationFrame(mainloop);
 }
 mainloop();
